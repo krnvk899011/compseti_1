1c621b11ba2ccdb281f292e5b2e197183f6c3abd
krnvk899011 <kkorneva2006@mail.ru>
2025-11-12 23:03:04 +0300

    Update serverBin.cpp

--- serverBin.cpp
+++ serverBin.cpp
@@ -1,148 +1,91 @@
-// serverBIN.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
 
 #include <iostream>
 #include <fstream>
-#include <windows.h>
+#include <string>
+#include <cstring>
+#include <thread>
+#include <chrono>
+
 using namespace std;
 
-struct Student
-{
-    char surname[25]; // фамилия студента
-    int grade1;       // оценка 1
-    int grade2;       // оценка 2
-    int grade3;       // оценка 3
-    int grade4;       // оценка 4
-} S;
+struct StudentRequest {
+    char name[50];
+    int g1, g2, g3, g4;
+};
 
-struct Answer
-{
-    bool hasDebt;                // есть ли задолженность
-    bool hasIncreasedScholarship; // повышенная стипендия
-    bool hasRegularScholarship;   // обычная стипендия
-} answer;
+struct StudentResponse {
+    char result[200];
+};
 
-long size_pred;
+int main() {
+    setlocale(LC_ALL, "Rus");
+    long lastSize = 0;
 
-int main()
-{
-    ifstream fR;
-    ofstream fA;
-    setlocale(LC_ALL, "rus");
-
-    char* nameR = "C:\\REQUEST.bin"; // бинарный файл запросов клиентов
-    char* nameA = "C:\\ANSWER.bin";  // бинарный файл ответов сервера
-
-    cout << "server is working" << endl;
-
-    // начальные установки
-    fR.open(nameR, ios::binary);
-    if (fR.is_open()) {
-        fR.seekg(0, ios::end);
-        size_pred = fR.tellg();
-        fR.close();
-    }
-    else {
-        size_pred = 0;
-    }
-
-    // начало работы сервера
-    while (true)
-    {
-        fR.open(nameR, ios::binary);
-        if (!fR.is_open()) {
-            Sleep(1000);
+    while (true) {
+        ifstream file("C:/Users/rain2/OneDrive/Рабочий стол/CompSeti/Client_bin/client.bin", ios::binary);
+        if (!file) {
+            // Если файл не существует, ждем и проверяем снова
+            this_thread::sleep_for(chrono::milliseconds(500));
             continue;
         }
 
-        fR.seekg(0, ios::end);
+        file.seekg(0, ios::end);
+        long currentSize = file.tellg();
+        file.close();
 
-        // есть новые запросы от клиентов?
-        while (size_pred >= fR.tellg())
-        {
-            Sleep(100);
-            fR.seekg(0, ios::end);
-        }
+        if (currentSize > lastSize) {
+            ifstream in("C:/Users/rain2/OneDrive/Рабочий стол/CompSeti/Client_bin/client.bin", ios::binary);
+            in.seekg(lastSize);
 
-        // получен новый запрос
-        fR.seekg(size_pred, ios::beg);
-        fR.read((char*)&S, sizeof(S));
-        size_pred = fR.tellg();
-        fR.close();
+            StudentRequest request;
+            while (in.read(reinterpret_cast<char*>(&request), sizeof(StudentRequest))) {
+                // Обработка запроса
+                StudentResponse response;
 
-        // обработка данных студента
-        bool hasDebt = false;
-        bool hasIncreasedScholarship = false;
-        bool hasRegularScholarship = false;
+                // Проверка корректности оценок
+                if (request.g1 > 5  request.g1 < 2  request.g2 > 5  request.g2 < 2 
+                    request.g3 > 5  request.g3 < 2  request.g4 > 5  request.g4 < 2) {
+                    strncpy_s(response.result, "Введите корректные значения от 2 до 5", sizeof(response.result) - 1);
+                    response.result[sizeof(response.result) - 1] = '\0';
+                }
+                // Проверка задолженностей
+                else if (request.g1 < 3  request.g2 < 3  request.g3 < 3  request.g4 < 3) {
+                    string result_str = string(request.name) + ": Есть задолженности";
+                    strncpy_s(response.result, result_str.c_str(), sizeof(response.result) - 1);
+                    response.result[sizeof(response.result) - 1] = '\0';
+                }
+                else {
+                    // Расчет стипендии
+                    double avg = (request.g1 + request.g2 + request.g3 + request.g4) / 4.0;
 
-        // проверка на задолженности и подсчет оценок
-        int grades[4] = { S.grade1, S.grade2, S.grade3, S.grade4 };
-        int excellentCount = 0;
-        int goodCount = 0;
-        int satisfactoryCount = 0;
+                    if (request.g1 < 4  request.g2 < 4  request.g3 < 4 || request.g4 < 4) {
+                        string result_str = string(request.name) + ": Нет стипендии, у студента есть отметка 3 (средний балл " + to_string(avg) + ")";
+                        strncpy_s(response.result, result_str.c_str(), sizeof(response.result) - 1);
+                        response.result[sizeof(response.result) - 1] = '\0';
+                    }
+                    else if (avg >= 4.5) {
+                        string result_str = string(request.name) + ": Повышенная стипендия (средний балл " + to_string(avg) + ")";
+                        strncpy_s(response.result, result_str.c_str(), sizeof(response.result) - 1);
+                        response.result[sizeof(response.result) - 1] = '\0';
+                    }
+                    else if (avg >= 4.0) {
+                        string result_str = string(request.name) + ": Обычная стипендия (средний балл " + to_string(avg) + ")";
+                        strncpy_s(response.result, result_str.c_str(), sizeof(response.result) - 1);
+                        response.result[sizeof(response.result) - 1] = '\0';
+                    }
+                    else {
+                        string result_str = string(request.name) + ": Нет стипендии (средний балл " + to_string(avg) + ")";
+                        strncpy_s(response.result, result_str.c_str(), sizeof(response.result) - 1);
+                        response.result[sizeof(response.result) - 1] = '\0';
+                    }
+                }
 
-        for (int i = 0; i < 4; i++) {
-            if (grades[i] == 2) {
-                hasDebt = true; // двойка - задолженность
-            }
-            else if (grades[i] == 3) {
-                satisfactoryCount++; // тройка
-            }
-            else if (grades[i] == 4) {
-                goodCount++; // четверка
-            }
-            else if (grades[i] == 5) {
-                excellentCount++; // пятерка
-            }
-        }
-
-        // расчет стипендии (только если нет задолженностей)
-        if (!hasDebt) {
-            if (excellentCount == 4) {
-                // все отлично - повышенная стипендия
-                hasIncreasedScholarship = true;
-            }
-            else if (satisfactoryCount == 0 && excellentCount + goodCount == 4) {
-                // нет троек, только 4 и 5 - обычная стипендия
-                hasRegularScholarship = true;
-            }
-            // если есть тройки - стипендии нет
-        }
-
-        // формирование ответа
-        answer.hasDebt = hasDebt;
-        answer.hasIncreasedScholarship = hasIncreasedScholarship;
-        answer.hasRegularScholarship = hasRegularScholarship;
-
-        // передача ответа клиенту
-        fA.open(nameA, ios::binary | ios::app);
-        fA.write((char*)&answer, sizeof(answer));
-        fA.close();
-
-        cout << "Обработан студент: " << S.surname
-            << " Оценки: " << S.grade1 << "," << S.grade2 << "," << S.grade3 << "," << S.grade4
-            << " Результат: ";
-        if (hasDebt) {
-            cout << "Задолженность";
-        }
-        else if (hasIncreasedScholarship) {
-            cout << "Повышенная стипендия";
-        }
-        else if (hasRegularScholarship) {
-            cout << "Обычная стипендия";
-        }
-        else {
-            cout << "Без стипендии";
-        }
-        cout << endl;
-    }
-    return 0;
-}
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
+                // Отправка ответа
+                ofstream out("C:/Users/rain2/OneDrive/Рабочий стол/CompSeti/Server_bin/server.bin", ios::binary | ios::app);
+                if (out) {
+                    out.write(reinterpret_cast<char*>(&response), sizeof(StudentResponse));
+                    out.close();
+                } 
 //   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
 //   2. В окне Team Explorer можно подключиться к системе управления версиями.
 //   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
